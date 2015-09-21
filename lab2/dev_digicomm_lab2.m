3050b1f492f3127337787491f8f735e6f1e63a35
Devin Trejo <dtrejod@gmail.com>
2015-09-21 00:56:58 -0400

    Lab 2 Code Finished
    Rewrote large portions of lab2 code. Now compares Actual, Simulated, and
    caculated values.

--- lab2/dev_digicomm_lab2.m
+++ lab2/dev_digicomm_lab2.m
@@ -7,10 +7,15 @@ clear; clc; close all;
 
 % Input parameters
 A = 1; % Amplitude of sine wave (V)
-N1 = 0.1E-3; % Time legnth of Ramp1
-N2 = 0.2E-3; % Time legnth of constant
-N3 = 0.25E-3;% Time legnth of Ramp2
+N1 = 0.1; % Time legnth of Ramp1
+N2 = 0.2; % Time legnth of constant
+N3 = 0.25;% Time legnth of Ramp2
 T = 1.5E-3;  % Period of waveform (secs)
+
+t0 = 0;
+t1 = N1*T;
+t2 = (N1+N2)*T;
+t3 = (N1+N2+N3)*T;
 
 % Simulation Specific Input
 Ts = 1E-6; % Sample Time Period
@@ -19,28 +24,35 @@ startTime = 0; % Simulation Start Time (sec) (Do not change from 0)
 stopTime = T; % Simulation Stop Time (sec) - Simulate over one period
 N = fs*T; % Samples in Simulation
 kVec = 0:5;
-spec_cf = 0;
-spec_span = Ts*max(kVec);
-
 
 % ----------------------------------------------------------
+% ----------------------------------------------------------
 % - Below this line everything is automatically calculated -
+% ----------------------------------------------------------
 % ----------------------------------------------------------
 
 % ------------------------------------
 % - Simulate Simulink Model (Part 2) -
 % ------------------------------------
 
+% Frequnecy Range in accordance with Nyquist
+f = linspace(0,fs,N);
+
+% Max freqeuncy to display on Spectrum Analyzer
+spec_span = f(length(kVec));
+
 % Run Simulink Simulation with given workspace parameters
 options = simset('SrcWorkspace','current');
-set_param('lab2', 'StartTime', num2str(startTime), 'StopTime', ...
+set_param('lab2_1', 'StartTime', num2str(startTime), 'StopTime', ...
     num2str(stopTime));
-sim('lab2',[],options);
+sim('lab2_1',[],options);
 
-% Extracted Values for first five values in Spectrum Analyzer (in dBm)
-Xsim = [0 36.371E-3 17.592E-3 4.646E-3 712E-6 281.039E-6 63.236E-6 ...
-    15.314E-6 81.757E-6 47.169E-6 10.184E-6 8.77E-6 3.008E-6 ...
-    1.586E-6 5.038E-6 ];
+% Extracted Values for first five values in Spectrum Analyzer (in Watts)
+Pw_XSim = [130.173E-3 161.838E-3 33.651E-3 1.781E-3 220.210E-6 ...
+    144.803E-6];
+% In terms of watts. We convert back to Volts (assume Rload = 1ohm)
+XSim = sqrt(Pw_XSim*2);
+XSim(1) = sqrt(Pw_XSim(1));
 
 % Extract Data from Simunlink Simulation. The simulation spits out one
 % extra sample so we truncate it by one. We also define the signal in terms
@@ -54,87 +66,101 @@ sigP = x(1:N); % Signal over one period
 
 % Find Fourier Series Complex Components. We define a inline function
 n = 0:N-1;
-a = @(k) (2/N) * sum(sigP.*cos(2*pi*k*n/N));
-b = @(k) (2/N) * sum(sigP.*sin(2*pi*k*n/N));
+ts = T/N;
+t = n*ts;
 
-% Lets solve for "c" for many different values of k by using a loop
-aVec = 0*kVec;
-bVec = 0*kVec;
-cVec2 = 0*kVec;
-i = 1;
-for k = 0:1:N-1
-    if k == 0
-        % For k=0 we have a special case we need to consider
-        aVec(i) = 1/N * sum(T);
-        bVec(i) = b(k);
-        cVec2(i)=aVec(i);
-        i = i + 1;
-        continue
-    end
-    aVec(i) = a(k);
-    bVec(i) = b(k);
-    % Next we want to convert to complex coefficents to compare the 
-    % two methods. We do this by finding the magnitude of the coefficients
-    X(i)=1/2*(aVec(i)-j*bVec(i));
-    i = i + 1;
-end
+% Define inline fn to calculate the Rectangular Fourier Series
+getA = @(n) (2/T)*sum(sigP.*cos(2*pi*n/T*t))*ts;
+getB = @(n) (2/T)*sum(sigP.*sin(2*pi*n/T*t))*ts;
+getAs = @(ns) arrayfun(getA,ns);
+getBs = @(ns) arrayfun(getB,ns);
+getXs = @(ns) sqrt(getAs(ns).^2 + getBs(ns).^2);
 
-% X is the double sided Freuqnewcy Spectra
+% Calculate the Fourier Series
+XCalc = getXs(kVec);
+XCalc(1) = XCalc(1)/2;
 
-% Frequnecy Range in accordance with Nyquist
-f = linspace(0,fs/2,N);
-
-figure()
-plot(f, pow2db(abs(X))+30);
-xlabel('Frequency (Hz)');
-ylabel('Power');
-%xlim([0 f(max(kVec)+1)]);
-grid on;
+% Find Actualy FFT
+XAct = 2*abs(fft(sigP)/N);
+XAct(1) = XAct(1)/2;
 
 % ----------------------------
 % - Calculate Power (Part 1) -
 % ----------------------------
 
-% Calculate Power two different ways
-Pav = 1/N*sum(sigP.^2); % Assume RL = 1 ohm
-PavdBm = pow2db(Pav) + 30; % Power calculated using Parsevals (in dBm)
-Pav2 = sum((abs(X)).^2);
-Pav2dBm = pow2db(Pav2) + 30;
+% Calculate Average Power for Actual, Simulated, and Calculated
+PavAct = mean(sigP.^2); % Assume RL = 1 ohm
+PavActdBm = pow2db(PavAct) + 30;
+PavSim = sum(XSim.^2);
+PavSimdBm = pow2db(PavSim) + 30;
+PwX = XCalc.^2/2;
+PwX(1) = XCalc(1).^2;
+PwX = cumsum(PwX);
+PavCalc = sum(PwX);
+PavCalcdBm = pow2db(PavCalc) + 30;
 
-% Normalized Average Power
-Pnorm = Pav2/Pav * 100;
+% Max Harmonic
+kMax = max(kVec)+1;
 
-fprintf(['Power in signal = %0.2f Watts / %0.2f dBm \n'...
-    'Power in signal as specified using Parserval''s theorem = '...
-    '%0.2f Watts / %0.2f dBm\n'], ...
-    Pav, PavdBm, Pav2, Pav2dBm);
-
-fprintf('Normalized Power = %0.2f %%\n', Pnorm);
+% Plot
+figure()
+plot(f(1:kMax),XCalc);
+xlabel('Frequency (Hz)');
+ylabel('Power (dBm)');
+xlim([0 f(max(kVec+1))]);
+grid on;
 
 % DC Value
-DC = sqrt(X(1));
+DC = mean(sigP);
+DC2 = XSim(1);
+DC3 = XCalc(1);
+
+% RMS Values
+RMS = rms(sigP);
+RMS2 = sqrt(mean(XSim(1).^2));
+RMS3 = sqrt(mean(XCalc(1).^2));
 
 % Print scope and spectrum analyzer to figures
 figure();
-plot(sig);
+plot(t, sigP);
 hold on
-plot(xlim, [DC DC], 'r');
+plot(xlim, [DC DC], 'r--');
+plot(xlim, [RMS RMS], 'm--');
 grid on
 xlabel('time (secs)');
 ylabel('Amplitude (V)');
-legend('Original Signal', 'DC Value');
+legend('Original Signal', 'DC Value', 'RMS Value');
 
 % -----------------------------
 % - Different in Coefficients -
 % -----------------------------
 
+% Find normalized power in each harmonic
+Pharm = PwX/PavAct*100;
+
 % Find Fourier Transform from Spectrum Analyzer and Calculated
-Per_err = abs(abs(Xsim(1:max(kVec)+1)-X(1:max(kVec)+1))./X(1:max(kVec)+1).*100);
+Per_err = abs(XSim-XCalc)./XCalc*100;
 
 % Print to console the findings
-tableLegend = {'f'; 'n'; 'X_Calc'; 'X_Sim'; 'Percent_Diff'};
-Presults = table(f(1:max(kVec)+1)',kVec(:),abs(X(1:max(kVec)+1))',...
-    abs(Xsim(1:max(kVec)+1))', Per_err', ...
+tableLegend = {'f'; 'n'; 'Norm_Power'; 'Pw_XAct'; 'Pw_XSim'; ...
+    'Pw_XCalc'; 'Percent_Diff'};
+Presults = table(f(1:kMax)', kVec(:), Pharm(:), XAct(1:kMax)', ...
+    XSim', XCalc', Per_err', ...
     'VariableNames', tableLegend);   
-    
-disp(Presults)
+disp(Presults);
+
+fprintf(['Actual: DC = %0.2f / RMS = %0.2f\n'...
+    'Simulated: DC = %0.2f / RMS = %0.2f\n'...
+    'Calculated: DC = %0.2f / RMS = %0.2f\n\n'], ...
+    DC, RMS, DC2, RMS2, DC3, RMS3);
+
+% Plot a comparison between simulated and calculated values. 
+figure()
+plot(kVec, XAct(1:kMax), 'LineWidth', 2);
+hold on
+plot(kVec, XSim);
+plot(kVec, XCalc);
+xlabel('harmonics (n)');
+ylabel('Volts (V)');
+legend('Actual', 'Simulated', 'Calculated');
+grid on
