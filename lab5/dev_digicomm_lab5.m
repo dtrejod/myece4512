6ede2934ba06ec327d66b7b890fe013becdec6cd
Devin Trejo <dtrejod@gmail.com>
2015-11-10 17:32:37 -0500

    Lab4 model submit.

--- lab5/dev_digicomm_lab5.m
+++ lab5/dev_digicomm_lab5.m
@@ -24,7 +24,7 @@ rb = 5E3; % Datarate bits/sec
 Tb = 1/rb; % Bit time
 Ts = Tb/100; % Sample Time Period (100 Samples per bit time)
 fs = 1/Ts; % Sample Frequency
-Threshold = 0;
+Threshold = A0^2*Tb;
 % Transmit Delay Time
 %
 delta = round(310E-6*fs);
@@ -35,21 +35,29 @@ delta = round(310E-6*fs);
 % Time Data
 %
 startTime = 0; % Simulation Start Time (sec) (Do not change from 0)
-stopTime = 10*Tb; % Simulation Stop Time (sec)
+stopTime = 1000*Tb; % Simulation Stop Time (sec)
 t = startTime:Ts:stopTime; % Time vector
 
 % Signals for binary 0/1
 %
-m0 = A0*sin(pi*t/Tb); % Representation of Binary 0
-m1 = A1*sin(pi*t/Tb); % Representation of Binary 1
+f0 = 1/(2*Tb);
+f1 = 1/(2*Tb);
+m0 = A0*sin(2*pi*f0*t); % Representation of Binary 0
+m1 = A1*sin(2*pi*f1*t); % Representation of Binary 1
 figure();
 plot(t, m0, t, m1);
 legend('m0','m1');
+
+% Power in A0/A1
+%
+P0 = A0^2*Tb
+P1 = A1^2*Tb
 
 % Carrier Signal
 %
 carrier = Ac*sin(2*pi*fc*t);
 reference = carrier;
+
 % Noise Power
 %
 sigma = 0;
